f36dc6627fcbff5a6703eee418392b42e1366f36
nardiniryan5-ai <nardiniryan5@gmail.com>
2026-09-14 09:47:41 -0300

    fase 1

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+startup = {
+    "nome": "CyberPulse Tech",
+    "segmento": "Segurança da Informação",
+    "ano_adesao": "2026"
+}
+solucoes_ativas =  ["Firewall IA", "Scan de Vulnerabilidades"]
+
+print("STARTUP:", startup["nome"],"Segmento:", startup["segmento"], )
+print("Projetos Ativos:", solucoes_ativas[0], )
+
+bancadas = [
+    [1,0],
+    [0,1]
+]
+print("status da bancada N1:", bancadas[0][0], "\nstatus da bancada N2:", bancadas[0][1])
+print("status da bancada S1:", bancadas[1][0], "\nstatus da bancada S2:", bancadas[1][1])
+print("Se status da bancada for 1 = ocupada, Se for 0 = livre.")
 
